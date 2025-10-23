e3c232bddb9d4a7ac770815ea13878e88787e8c0
colaholiker <colaholiker@waskurzes.de>
2025-10-23 15:45:56 +0200

    blub

--- Configuration/Extbase/Persistence/Classes.php
+++ Configuration/Extbase/Persistence/Classes.php
@@ -8,7 +8,7 @@ declare(strict_types=1);
  * https://docs.typo3.org/c/typo3/cms-core/master/en-us/Changelog/10.0/Breaking-87623-ReplaceConfigpersistenceclassesTyposcriptConfiguration.html
  */
 return [
-    SchachvereinBalingenEv\NuToNews\Domain\Category::class => [
+    SchachvereinBalingenEv\NuToNews\Domain\Model\Category::class => [
         'tableName' => 'sys_category',
     ],
 ];
