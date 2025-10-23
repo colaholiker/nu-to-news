2036fd25c8660e9683b871bfc688fb2ad382cd96
colaholiker <colaholiker@waskurzes.de>
2025-10-23 14:48:26 +0200

    blub

--- Classes/Domain/Model/Category.php
+++ Classes/Domain/Model/Category.php
@@ -1,5 +1,5 @@
 <?php
-namespace SchachvereinBalingenEv\NuToNews\Domain\Model
+namespace SchachvereinBalingenEv\NuToNews\Domain\Model;
 
 class Category extends \TYPO3\CMS\Extbase\Domain\Model\Category {}
 
