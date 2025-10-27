cd04bd6a4c17c8ad80cd75f8cab8baf0ef90d035
colaholiker <colaholiker@waskurzes.de>
2025-10-27 15:14:57 +0100

    blub

--- Classes/Task/NuToNews.php
+++ Classes/Task/NuToNews.php
@@ -158,7 +158,7 @@ final class NuToNews extends AbstractTask
                 $news->setHidden(false);
                 $news->setAuthor('svw.info');
                 $news->setAuthorEmail('webmaster@svbalingen.de');
-                $news->setCategory($category);
+                $news->setCategories($category);
                 echo "write";
             }
 
