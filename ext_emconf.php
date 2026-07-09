93bbb7b7e3c502949ed00d5252a9a8c5e49a6be4
colaholiker <colaholiker@waskurzes.de>
2026-07-09 10:32:18 +0200

    v14 support add

--- ext_emconf.php
+++ ext_emconf.php
@@ -6,6 +6,7 @@ $EM_CONF[$_EXTKEY] = [
     'constraints' => [
         'depends' => [
             'typo3' => '13.4.0-14.4.99',
+            'scheduler' => '13.4.0-14.4.99',
             'news' => '12.2.0-14.99.99',
         ],
     ],
