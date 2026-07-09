1419555c3162f1be20b3eb60cbc31cd0464407aa
colaholiker <colaholiker@waskurzes.de>
2026-07-09 10:19:40 +0200

    v14 support add

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,8 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'NuLiga Parser to tx_news',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.0.0-13.4.99',
+            'typo3' => '13.4.0-14.4.99',
+            'news' => '12.2.0-14.99.99',
         ],
     ],
     'autoload' => [
